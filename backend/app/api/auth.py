50c6e9c3bc0f02f53957fa59221e1e594d48a496
shahil04 <ansarirahish455@gmail.com>
2026-09-06 11:06:41 +0530

    add JWT Authentication api in api/auth.py

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -76,3 +76,56 @@ def login_user(
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+
+# JWT Authentication
+# Now we need to protect APIs.
+
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from jose import jwt
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={
+            "WWW-Authenticate": "Bearer"
+        }
+    )
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            JWT_SECRET_KEY,
+            algorithms=[JWT_ALGORITHM]
+        )
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+
+    except JWTError:
+
+        raise credentials_exception
+
+    user = (
+        db.query(User)
+        .filter(
+            User.id == int(user_id)
+        )
+        .first()
+    )
+
+    if user is None:
+        raise credentials_exception
+
+    return user
